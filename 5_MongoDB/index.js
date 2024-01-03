b5e6749e8d0f9b8b8dabfff5da71e0fbb458ef9a
CodeInsideU <vratik.zade.15@gmail.com>
2024-01-03 11:50:34 +0530

    Done with the tutorial

--- 5_MongoDB/index.js
+++ 5_MongoDB/index.js
@@ -6,10 +6,21 @@ mongoose.connect('mongodb://127.0.0.1/testDatabase')
 
 // Schema
 const courseSchema = new mongoose.Schema({
-    name: String,
-    creator: String,
+    name: {type: String, required: true, minlength: 5, maxlength: 200},
+    tags: {type : Array, validate : {
+        validator : function(tags){
+            return tags.length > 1
+        }
+    }},
+    category: {
+        type: String,
+        required : true,
+        enum : ['DSA', 'WEB', 'Mobile', 'Data Science']
+    },
+    creator: {type: String, required: true},
     publishDate: {type: Date, default: Date.now},
-    isPublished: Boolean
+    isPublished: {type: Boolean, required: true},
+    rating: {type: Number, required: function(){return this.isPublished}}
 })
 
 // Model
@@ -17,19 +28,76 @@ const Course = mongoose.model('Course', courseSchema)
 
 async function createCourse(){
     const course = new Course({
-        name: 'Java',
-        creator: 'ABC',
+        name: 'MongoDb',
+        creator: 'John Doe',
+        category: 'DSA',
+        tags: ['express'],
+        rating: 4.5,
         isPublished: false
+        // creator: 'DEF',
+        // isPublished: true,
+        // rating: 3.0
     })
+    try{
+        await course.validate()
+    //     const result = await course.save()
+    // console.log(result)
+    }
+    catch(error){
+        // console.error(error.message)
+        for (field in error.errors){
+            console.log(error.errors[field])
+        }
+    }
 
-    const result = await course.save()
-    console.log(result)
-}
+    
+}// creating
 
+// Ratings 0 to 5
+// eq(equal)
+
+// gt(greater than)
+// gte(greater than and equal to)
+// lt
+// lte
+// in
+// not in
+
+// logical operator
+// or
+// and
 async function getCourses(){
-    const courses = await Course.find({creator: 'Vratik'}).select({name: 1, publishDate: 1}).sort({name: -1})
+    // const courses = await Course.find({rating: {$gte : 4}}).select({name: 1, publishDate: 1})
+    const courses = await Course.find({rating: {$in: [3,4.5]}}).select({name: 1, publishDate: 1}).and([{creator: 'DEF'},{rating:3}])
     console.log(courses)
-}
+}//reading course
+
+
+
+
 
 // createCourse()
-getCourses()
+// getCourses()
+
+async function updateCourse(id){
+    let course = await Course.findById(id)
+    if(!course) return;
+
+    course.name = 'Python'
+    course.creator = 'Steve'
+
+    const updateCourse = await course.save()
+    console.log(updateCourse)
+
+}//update course
+
+// updateCourse('65942374a680f85280da72dd')
+
+// Deleting
+async function deleteCourse(id){
+    let course = await Course.findByIdAndDelete(id)
+    console.log(course)
+}
+// deleteCourse('658fab9564d831065e270ceb')
+
+createCourse()
---
+++ 6_project/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const categories = require('./Routes/categories')
+const students = require('./Routes/students')
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1/learningPlatform')
+.then(()=> console.log('Connection is successful to database'))
+.catch(err => console.error('Couldnt connect to mongodb', err))
+
+app.use(express.json())
+app.use(categories)
+app.use('/api/categories', categories)
+app.use('/api/students', students)
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}....`))
